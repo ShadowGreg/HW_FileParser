a104e1c686282758520c2f530dea99ec44367f12
Greg Osetrov <88874859+ShadowGreg@users.noreply.github.com>
2026-04-07 13:45:59 +0300

    рефакторинг EventSaveDataProsessor в EventSaveDataProcessor

---
+++ HW FileParser/Service/EventSaveDataProcessor.cs
@@ -0,0 +1,14 @@
+using HW_FileParser.Entities.DTO;
+using HW_FileParser.Service.Abstractions;
+
+namespace HW_FileParser.Service;
+public class EventSaveDataProcessor(
+    IDataContext db
+    ): IEventHandler<DownloadResult>
+{
+    public async Task<Guid> WriteDataAsync(DownloadResult downloadResult, CancellationToken ct = default) {
+        return await db.WriteDataAsync(downloadResult, ct);
+    }
+
+    public async Task HandleAsync(DownloadResult @event) => await WriteDataAsync(@event, CancellationToken.None);
+}
